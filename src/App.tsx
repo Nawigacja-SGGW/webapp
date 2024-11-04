369dccc911598b47138dd0967d1c2a1c12c81db4
mchodkowski <mchodkowski@deloittece.com>
2024-11-04 15:43:07 +0100

    Added basic routing, removed initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 function App() {
-  return (
-    <>
-      <h1>sggw navigation webapp</h1>
-    </>
-  );
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <div>Home page/Map page</div>,
+        },
+        {
+            path: '/auth',
+            element: <div>Auth page</div>,
+        },
+        {
+            path: '/settings',
+            element: <div>Settings page</div>,
+        },
+        {
+            path: '/profile',
+            element: <div>Profile page</div>,
+        },
+        // TODO: Add a route for specific object like /object/id
+        {
+            path: '/objects',
+            element: <div>Home page</div>,
+        },
+        {
+            path: '*',
+            element: <div>Redirect do home page?</div>,
+        },
+    ]);
+    return <RouterProvider router={router} />;
 }
 
 export default App;
