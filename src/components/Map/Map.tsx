c6ca4be5dee0cc647c20e6b74af1b0c232b8a894
MariuszAKS <mariusz-k-sikora@o2.pl>
2024-11-04 20:29:15 +0100

    feat: added a custom marker (icons) and one centered on WZIM

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,23 @@
 import L from 'leaflet';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './leaflet.css';
 import './Map.scss';
+
+const markerIcon = new L.Icon({
+  iconUrl: '../../../public/marker-icon.png',
+  iconRetinaUrl: '../../../public/marker-icon-2x.png',
+  shadowUrl: '../../../public/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -24],
+  shadowSize: [41, 41],
+});
 
 export const Map = () => {
   const sw = L.latLng(52.15656, 21.03624);
   const ne = L.latLng(52.1674, 21.05596);
+
+  const wzimCoords = L.latLng(52.16198, 21.04633);
 
   return (
     <div className="map-container">
@@ -21,6 +33,10 @@ export const Map = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
+
+        <Marker position={wzimCoords} icon={markerIcon}>
+          <Popup>WZIM</Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
