f421eb9fcc32511086cdf04ba25c24c549169808
Tomasz Budny <tomaszbudny01@gmail.com>
2024-11-05 21:29:34 +0100

    add page margin (#14)

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -5,7 +5,7 @@ export const Settings = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
+    <div className="settings">
       <h1>{t('settingsPage.header')}</h1>
       <div>{t('exampleTranslation')}</div>
     </div>
