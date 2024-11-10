d27481432b3c3359f0e2e71f3a2d933a61858a68
MariuszAKS <mariusz-k-sikora@o2.pl>
2024-11-10 14:40:22 +0100

    Added displaying forwarded places and path (right now using mock of places and path between them, also removed bounds and min zoom temporarily)

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,11 @@
-import L from 'leaflet';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import L, { LatLng } from 'leaflet';
+import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
 import { useMapEvents } from 'react-leaflet';
 import CustomMarker from './CustomMarker';
 import './leaflet.css';
 import './Map.scss';
+
+import { demo_places, Place } from '../../mocks/places.ts';
 
 function OnMapClick() {
   useMapEvents({
@@ -15,20 +17,37 @@ function OnMapClick() {
   return null;
 }
 
-export const Map = () => {
+export const Map = ({ places, path }: { places?: Place[]; path?: L.LatLng[] }) => {
   const sw = L.latLng(52.15656, 21.03624);
   const ne = L.latLng(52.1674, 21.05596);
 
-  const wzimCoords = L.latLng(52.16198, 21.04633);
+  // const wzimCoords = L.latLng(52.16198, 21.04633);
+
+  // Following lines for testing only
+  if (!places) {
+    places = demo_places;
+  }
+  if (!path) {
+    path = places.map((place) => new L.LatLng(Number(place.lat), Number(place.lon)));
+  }
+  // end of lines for testing
+
+  let route: [L.LatLng, L.LatLng][] = [];
+  if (path && path.length > 1) {
+    // route = path.slice(0, -1).map((_, i) => [path[i], path[i + 1]])
+    for (let i = 0; i < path.length - 1; i++) {
+      route.push([path[i], path[i + 1]]);
+    }
+  }
 
   return (
     <div className="map-container">
       <MapContainer
         center={[52.16256, 21.04219]}
-        maxBounds={L.latLngBounds(sw, ne)}
+        //maxBounds={L.latLngBounds(sw, ne)}
         maxBoundsViscosity={1}
         zoom={16}
-        minZoom={16}
+        //minZoom={16}
         maxZoom={18}
       >
         <TileLayer
@@ -36,7 +55,15 @@ export const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
 
-        <CustomMarker position={wzimCoords} text={'WZIM'} />
+        {places?.map((place, index) => (
+          <CustomMarker
+            key={index}
+            position={L.latLng(Number(place.lat), Number(place.lon))}
+            text={place.name + ' - ' + place.description}
+          />
+        ))}
+
+        <Polyline positions={route} />
 
         <OnMapClick />
       </MapContainer>
