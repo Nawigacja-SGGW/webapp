eab374fb3dbb49ec6d4cdeecf93e8ea45cf727ca
Tomasz Budny <tomaszbudny01@gmail.com>
2024-12-07 23:13:46 +0100

    add radio options to settings page (#50)

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -4,7 +4,7 @@ import { FormLayout } from '../../layouts/AuthorizationFormLayout/AuthorizationF
 import { Input } from '../../components/ui/Input/Input';
 import { Button } from '../../components/ui/Button/Button';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { RadioInputs } from '../ui/RadioInputs/RadioInputs';
+import { RadioInputAttributes, RadioInputs } from '../ui/RadioInputs/RadioInputs';
 
 export const Settings = () => {
   const { t } = useTranslation();
@@ -32,20 +32,36 @@ export const Settings = () => {
     }
   };
 
+  const routePreferencesOptions: RadioInputAttributes[] = [
+    { value: 'walk', label: 'Walk', id: 'option-walk' },
+    { value: 'bike', label: 'Bike', id: 'option-bike' },
+  ];
+
+  const appLanguageOptions: RadioInputAttributes[] = [
+    { value: 'polish', label: 'Polish', id: 'option-polish' },
+    { value: 'english', label: 'English', id: 'option-english' },
+  ];
+
   return (
     <div className="settings">
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormLayout title={t('settingsPage.header')}>
           <h3>{t('settingsPage.section.routePreferences')}</h3>
+          <br />
           <RadioInputs
-            radioInputsValues={[
-              { value: 'walk', label: 'Walk', id: 'walk' },
-              { value: 'cycle', label: 'Cycle', id: 'cycle' },
-            ]}
+            radioInputsValues={routePreferencesOptions}
+            registerOptions={register('routePreferences', { required: true })}
+          ></RadioInputs>
+
+          <br />
+          <h3>{t('settingsPage.section.appLanguage')}</h3>
+          <br />
+          <RadioInputs
+            radioInputsValues={appLanguageOptions}
             registerOptions={register('appLanguage', { required: true })}
           ></RadioInputs>
-          <h3>{t('settingsPage.section.appLanguage')}</h3>
 
+          <br />
           <h3>{t('settingsPage.section.changeYourPassword')}</h3>
           <p className="input-label">{t('authPage.labels.password')}</p>
           <Input
