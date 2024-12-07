044af09ce94a6160a62f1a064a4d17c29d15c493
Tomasz Budny <tomaszbudny01@gmail.com>
2024-12-07 20:39:08 +0100

    adjust settings page structure, add translations (#50)

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,20 +1,68 @@
 import './Settings.scss';
 import { useTranslation } from 'react-i18next';
+import { FormLayout } from '../../layouts/AuthorizationFormLayout/AuthorizationFormLayout';
+import { Input } from '../../components/ui/Input/Input';
+import { Button } from '../../components/ui/Button/Button';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 export const Settings = () => {
   const { t } = useTranslation();
 
+  type SettingsInputs = {
+    routePreferences: string;
+    appLanguage: string;
+    password: string;
+    confirmPassword: string;
+  };
+
+  const {
+    formState: { errors, isSubmitting, isValid },
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+  } = useForm<SettingsInputs>();
+
+  const onSubmit: SubmitHandler<SettingsInputs> = (data) => {
+    clearErrors();
+    const { password, confirmPassword } = data;
+    if (password !== confirmPassword) {
+      setError('confirmPassword', { type: 'manual', message: 'Passwords do not match' });
+    }
+  };
+
   return (
     <div className="settings">
-      <h1>{t('settingsPage.header')}</h1>
-      <br />
-      <h3>{t('settingsPage.yourData')}</h3>
-      <br />
-      <p>E-mail</p>
-      <p>sXXXXXX@sggw.edu.pl</p>
-      <br />
-      <p>Username</p>
-      <p>XXXXXXXX</p>
+      <FormLayout title={t('settingsPage.header')}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h3>{t('settingsPage.section.routePreferences')}</h3>
+          <h3>{t('settingsPage.section.appLanguage')}</h3>
+          <p className="input-label">{t('authPage.labels.password')}</p>
+          <Input
+            placeholder={t('settingsPage.input.placeholder.newPassword')}
+            type="password"
+            {...register('password', { required: true })}
+          />
+          {errors.password && <span className="input-error">This field is required</span>}
+          <p className="input-label">{t('authPage.labels.confirmPassword')}</p>
+          <Input
+            placeholder={t('settingsPage.input.placeholder.confirmPassword')}
+            type="password"
+            {...register('confirmPassword', { required: true })}
+          />
+        </form>
+      </FormLayout>
+      <div className="submit-btn-wrapper">
+        <div>
+          <Button
+            className="green-button submit-btn"
+            label={t('settingsPage.button.save')}
+            type="submit"
+            disabled={isSubmitting || !isValid}
+            primary
+          />
+        </div>
+      </div>
     </div>
   );
 };
