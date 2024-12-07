578a952762ee0adc1228de1b4a13ea6bedc63345
przemekpia <przemekpia@gmail.com>
2024-12-07 18:32:49 +0100

    feat: Sorting section

--- src/pages/ObjectsOverview/ObjectsOverviewPage.tsx
+++ src/pages/ObjectsOverview/ObjectsOverviewPage.tsx
@@ -4,8 +4,8 @@ import { Place } from '../../mocks/places.ts';
 import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { Object } from './components/Object/Object.tsx';
 import { Input } from '../../components/ui/Input/Input.tsx';
+import { Sliders } from '@styled-icons/bootstrap/Sliders';
 import { debounce } from 'lodash';
-
 import './ObjectsOverviewPage.scss';
 
 export const ObjectsOverviewPage = () => {
@@ -14,7 +14,13 @@ export const ObjectsOverviewPage = () => {
   const [places, setPlaces] = useState<Place[]>([]);
   const [initialPlaces, setInitialPlaces] = useState<Place[]>([]);
 
+  const [isSectionVisible, setIsSectionVisible] = useState(false);
+
   const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const toggleSectionVisibility = () => {
+    setIsSectionVisible((prev) => !prev);
+  };
 
   useEffect(() => {
     fetch('/objects')
@@ -42,15 +48,53 @@ export const ObjectsOverviewPage = () => {
     return () => debounceFilterPlaces.cancel();
   }, [debounceFilterPlaces]);
 
+  const handleSort = (criteria: 'az' | 'za') => {
+    const sortedPlaces = [...places].sort((a, b) => {
+      if (criteria === 'az') {
+        return a.name.localeCompare(b.name);
+      }
+      if (criteria === 'za') {
+        return b.name.localeCompare(a.name);
+      }
+      return 0;
+    });
+    setPlaces(sortedPlaces);
+  };
+
   return (
     <PageContentWrapper noHorizontalPadding noPadding>
       <div className="objects-overview-heading">
-        <Input
-          placeholder={t('objectsOverviewPage.search.placeholder')}
-          onChange={debounceFilterPlaces}
-          ref={inputRef}
-        />
+        <div className="search-container">
+          <Input
+            placeholder={t('objectsOverviewPage.search.placeholder')}
+            onChange={debounceFilterPlaces}
+            ref={inputRef}
+          />
+          <Sliders
+            className="search-icon"
+            size="20"
+            onClick={toggleSectionVisibility}
+            style={{ cursor: 'pointer' }}
+          />
+        </div>
       </div>
+      {isSectionVisible && (
+        <div className="set-section">
+          <div className="sort-section">
+            <p className="sort-title">{t('objectsOverviewPage.sortBy')}</p>
+            <div className="sort-options">
+              <label>
+                <input type="checkbox" onChange={() => handleSort('az')} />
+                {t('objectsOverviewPage.sort.az')}
+              </label>
+              <label>
+                <input type="checkbox" onChange={() => handleSort('za')} />
+                {t('objectsOverviewPage.sort.za')}
+              </label>
+            </div>
+          </div>
+        </div>
+      )}
       <section className="objects-list">
         {places.map((place: Place, idx) => (
           <Object
