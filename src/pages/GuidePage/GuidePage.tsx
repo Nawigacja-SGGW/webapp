2fe0cc3362317e170cb6806b388df0d699f73013
Tomasz Budny <tomaszbudny01@gmail.com>
2024-12-14 23:10:18 +0100

    extract reusable consts and styles (#61)

--- src/pages/GuidePage/GuidePage.tsx
+++ src/pages/GuidePage/GuidePage.tsx
@@ -1,3 +1,54 @@
+import L from 'leaflet';
+import { MapContainer, Polyline, TileLayer } from 'react-leaflet';
+// import { Polyline } from 'react-leaflet';
+
+// import CustomMarker from './CustomMarker';
+
+// import './leaflet.css';
+// import './Map.scss';
+
+import '../../leaflet.css';
+import {
+  BORDER_NE,
+  BORDER_SW,
+  MAP_CENTER,
+  MAP_MAX_BOUNDS_VISCOSITY,
+  MAP_MAX_ZOOM,
+  MAP_MIN_ZOOM,
+  MAP_ZOOM,
+  PathInfo,
+} from '../../components/Map/utils';
+import { useState } from 'react';
+
 export const GuidePage = () => {
-  return <div></div>;
+  const INITIAL_PATH_INFO: PathInfo = {
+    path: [],
+    totalTime: 0,
+    totalDistance: 0,
+    transportationMode: '',
+    nextManeuver: '',
+    nextManeuverModifier: '',
+    distanceUntillNextDirection: 0,
+  };
+
+  const [pathInfo, setPathInfo] = useState<PathInfo>(INITIAL_PATH_INFO);
+
+  return (
+    <div className="map-container">
+      <MapContainer
+        center={MAP_CENTER}
+        maxBounds={L.latLngBounds(BORDER_SW, BORDER_NE)}
+        maxBoundsViscosity={MAP_MAX_BOUNDS_VISCOSITY}
+        zoom={MAP_ZOOM}
+        minZoom={MAP_MIN_ZOOM}
+        maxZoom={MAP_MAX_ZOOM}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Polyline positions={pathInfo.path} />
+      </MapContainer>
+    </div>
+  );
 };
