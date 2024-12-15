538726303024dd8ac2fe1841313006f3c4aef87d
michalmlk <michal.melka1@gmail.com>
2024-12-15 10:44:30 +0100

    validation on register page

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -2,8 +2,9 @@ import { FormLayout } from '../../layouts/AuthorizationFormLayout/AuthorizationF
 import { Input } from '../../components/ui/Input/Input';
 import { Button } from '../../components/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm, useWatch } from 'react-hook-form';
 import { EMAIL_REGEXP } from '../../utils/constans.ts';
+import { useEffect } from 'react';
 
 export const RegisterPage = () => {
   const { t } = useTranslation();
@@ -16,59 +17,79 @@ export const RegisterPage = () => {
   };
 
   const {
-    formState: { errors, isSubmitting, isValid },
+    control,
+    formState: { errors, isValid, isSubmitting },
     register,
     handleSubmit,
-    setError,
-    clearErrors,
-  } = useForm<RegistrationInputs>();
+  } = useForm<RegistrationInputs>({
+    reValidateMode: 'onChange',
+    mode: 'onChange',
+  });
 
   const onSubmit: SubmitHandler<RegistrationInputs> = (data) => {
-    clearErrors();
-    const { password, confirmPassword } = data;
-    if (password !== confirmPassword) {
-      setError('confirmPassword', { type: 'manual', message: 'Passwords do not match' });
-    }
+    console.log(data);
   };
+
+  // testing purposes
+  useEffect(() => {
+    console.log(errors);
+  });
+
+  const passwordField = useWatch({ control, name: 'password' });
 
   return (
     <FormLayout title={t('authPage.title.signup')}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <p className="input-label">{t('authPage.labels.email')}</p>
         <Input
-          placeholder="sXXXXXX@sggw.edu.pl"
           {...register('email', {
             required: 'Email is invalid',
-            pattern: EMAIL_REGEXP,
+            pattern: {
+              value: EMAIL_REGEXP,
+              message: t('authPage.validation.invalidEmail'),
+            },
           })}
         />
-        {errors.email && <span className="input-error">This field is required</span>}
+        {errors.email ? (
+          errors.email.type === 'required' ? (
+            <span className="input-error">{t('authPage.validation.required')}</span>
+          ) : errors.email.type === 'pattern' ? (
+            <span className="input-error">{errors.email.message}</span>
+          ) : null
+        ) : null}
         <p className="input-label">{t('authPage.labels.username')}</p>
-        <Input placeholder="xxxxx" {...register('userName', { required: true })} />
-        {errors.userName && <span className="input-error">This field is required</span>}
+        <Input {...register('userName', { required: true })} />
+        {errors.userName?.type === 'required' && (
+          <span className="input-error">{t('authPage.validation.required')}</span>
+        )}
         <p className="input-label">{t('authPage.labels.password')}</p>
-        <Input
-          placeholder="••••••••"
-          type="password"
-          {...register('password', { required: true })}
-        />
-        {errors.password && <span className="input-error">This field is required</span>}
+        <Input type="password" {...register('password', { required: true })} />
+        {errors.password?.type === 'required' && (
+          <span className="input-error">{t('authPage.validation.required')}</span>
+        )}
         <p className="input-label">{t('authPage.labels.confirmPassword')}</p>
         <Input
-          placeholder="••••••••"
           type="password"
-          {...register('confirmPassword', { required: true })}
+          {...register('confirmPassword', {
+            required: true,
+            validate: (value) =>
+              !passwordField ||
+              value === passwordField ||
+              t('authPage.validation.passwordNotMatch'),
+          })}
         />
-        {errors.confirmPassword && <span className="input-error">This field is required</span>}
-        {errors.confirmPassword && errors.confirmPassword && (
+        {errors.confirmPassword?.type === 'required' && (
+          <span className="input-error">{t('authPage.validation.required')}</span>
+        )}
+        {errors.confirmPassword?.type === 'validate' && (
           <span className="input-error">{errors.confirmPassword.message}</span>
         )}
         <Button
           className="green-button"
           label={t('authPage.button.signup')}
           type="submit"
-          disabled={isSubmitting || !isValid}
           primary
+          disabled={!isValid || isSubmitting}
         />
       </form>
     </FormLayout>
