8e462fd3cae926e1ea343fba00da18744a1ac271
MariuszAKS <mariusz-k-sikora@o2.pl>
2024-12-19 12:25:49 +0100

    Commented whole Map component

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -51,115 +51,115 @@ const INITIAL_PATH_INFO: PathInfo = {
 };
 
 export const Map = () => {
-  const [points, setPoints] = useState(INITIAL_POINTS);
-  const [pathInfo, setPathInfo] = useState<PathInfo>(INITIAL_PATH_INFO);
+  // const [points, setPoints] = useState(INITIAL_POINTS);
+  // const [pathInfo, setPathInfo] = useState<PathInfo>(INITIAL_PATH_INFO);
 
-  const [mapState, setMapState] = useState<MapState>('browsing');
-  const [allLocations, setAllLocations] = useState<PlaceObject[]>([]);
+  // const [mapState, setMapState] = useState<MapState>('browsing');
+  // const [allLocations, setAllLocations] = useState<PlaceObject[]>([]);
 
-  const route_type = useAppStore((state: AppState) => state.preferences.routePreference);
+  // const route_type = useAppStore((state: AppState) => state.preferences.routePreference);
 
-  // Fetch all objects
-  // when first loading Map
-  useEffect(() => {
-    getObjectsList().then((data) => {
-      if (Array.isArray(data)) {
-        setAllLocations(data);
-      }
-    });
-  }, []);
-
-  // Update path (and path info)
-  // when points changes
-  useEffect(() => {
-    // Canceling navigation when too close to destination
-    if (mapState === 'navigating' && pathInfo.totalDistance < 5) {
-      setMapState('browsing');
-      setPoints(INITIAL_POINTS);
-      setPathInfo(INITIAL_PATH_INFO);
-    } else if (points?.destinationPoint && (points.startingPoint || points.locationCoords)) {
-      let startingPoint: L.LatLng | undefined;
-      let destinationPoint: L.LatLng = {
-        lat: Number(points.destinationPoint.latitude),
-        lng: Number(points.destinationPoint.longitude),
-      } as L.LatLng;
-
-      if (points.startingPoint) {
-        startingPoint = {
-          lat: Number(points.startingPoint.latitude),
-          lng: Number(points.startingPoint.longitude),
-        } as L.LatLng;
-      } else if (points.locationCoords) {
-        startingPoint = points.locationCoords;
-      }
-
-      if (startingPoint) {
-        getPath(startingPoint, destinationPoint, route_type).then((data) => setPathInfo(data));
-      }
-    } else {
-      setPathInfo(INITIAL_PATH_INFO);
-    }
-  }, [points]);
-
+  // // Fetch all objects
+  // // when first loading Map
   // useEffect(() => {
-  //   if (mapState === 'navigating') {
-  //     // Below solution when using real location (update on a timer, could add if location changed enough)
-  //     const interval = setInterval(() => {
-  //       getPath(
-  //         {
-  //           lat: points.locationPoint!.lat,
-  //           lng: points.locationPoint!.lng,
-  //         } as L.LatLng,
-  //         {
-  //           lat: points.destinationObject!.lat,
-  //           lng: points.destinationObject!.lng,
-  //         } as L.LatLng,
-  //         'foot'
-  //       ).then((data) => setPathInfo(data));
-  //     }, 1000);
-  //     return () => clearInterval(interval);
-  //   }
+  //   getObjectsList().then((data) => {
+  //     if (Array.isArray(data)) {
+  //       setAllLocations(data);
+  //     }
+  //   });
   // }, []);
 
-  const PopulateWithMarkers = () => {
-    return Array.isArray(allLocations)
-      ? allLocations.map((location, i) => (
-          <CustomMarker
-            position={L.latLng(Number(location.latitude), Number(location.longitude))}
-            onClick={() => OnMarkerClick(location)}
-            key={i}
-          />
-        ))
-      : null;
-  };
+  // // Update path (and path info)
+  // // when points changes
+  // useEffect(() => {
+  //   // Canceling navigation when too close to destination
+  //   if (mapState === 'navigating' && pathInfo.totalDistance < 5) {
+  //     setMapState('browsing');
+  //     setPoints(INITIAL_POINTS);
+  //     setPathInfo(INITIAL_PATH_INFO);
+  //   } else if (points?.destinationPoint && (points.startingPoint || points.locationCoords)) {
+  //     let startingPoint: L.LatLng | undefined;
+  //     let destinationPoint: L.LatLng = {
+  //       lat: Number(points.destinationPoint.latitude),
+  //       lng: Number(points.destinationPoint.longitude),
+  //     } as L.LatLng;
 
-  // Setting clicked marker's object as destination
-  const OnMarkerClick = (markerObject: PlaceObject) => {
-    if (mapState !== 'navigating') {
-      setPoints({
-        ...points,
-        destinationPoint: markerObject,
-      });
-    }
-  };
+  //     if (points.startingPoint) {
+  //       startingPoint = {
+  //         lat: Number(points.startingPoint.latitude),
+  //         lng: Number(points.startingPoint.longitude),
+  //       } as L.LatLng;
+  //     } else if (points.locationCoords) {
+  //       startingPoint = points.locationCoords;
+  //     }
 
-  // Setting location when clicked on map (not marker)
-  function OnMapClick() {
-    useMapEvents({
-      click: (event) => {
-        setPoints({
-          ...points,
-          locationCoords: event.latlng,
-          startingPoint: null,
-        });
-      },
-    });
-    return null;
-  }
+  //     if (startingPoint) {
+  //       getPath(startingPoint, destinationPoint, route_type).then((data) => setPathInfo(data));
+  //     }
+  //   } else {
+  //     setPathInfo(INITIAL_PATH_INFO);
+  //   }
+  // }, [points]);
+
+  // // useEffect(() => {
+  // //   if (mapState === 'navigating') {
+  // //     // Below solution when using real location (update on a timer, could add if location changed enough)
+  // //     const interval = setInterval(() => {
+  // //       getPath(
+  // //         {
+  // //           lat: points.locationPoint!.lat,
+  // //           lng: points.locationPoint!.lng,
+  // //         } as L.LatLng,
+  // //         {
+  // //           lat: points.destinationObject!.lat,
+  // //           lng: points.destinationObject!.lng,
+  // //         } as L.LatLng,
+  // //         'foot'
+  // //       ).then((data) => setPathInfo(data));
+  // //     }, 1000);
+  // //     return () => clearInterval(interval);
+  // //   }
+  // // }, []);
+
+  // const PopulateWithMarkers = () => {
+  //   return Array.isArray(allLocations)
+  //     ? allLocations.map((location, i) => (
+  //         <CustomMarker
+  //           position={L.latLng(Number(location.latitude), Number(location.longitude))}
+  //           onClick={() => OnMarkerClick(location)}
+  //           key={i}
+  //         />
+  //       ))
+  //     : null;
+  // };
+
+  // // Setting clicked marker's object as destination
+  // const OnMarkerClick = (markerObject: PlaceObject) => {
+  //   if (mapState !== 'navigating') {
+  //     setPoints({
+  //       ...points,
+  //       destinationPoint: markerObject,
+  //     });
+  //   }
+  // };
+
+  // // Setting location when clicked on map (not marker)
+  // function OnMapClick() {
+  //   useMapEvents({
+  //     click: (event) => {
+  //       setPoints({
+  //         ...points,
+  //         locationCoords: event.latlng,
+  //         startingPoint: null,
+  //       });
+  //     },
+  //   });
+  //   return null;
+  // }
 
   return (
     <div className="map-container">
-      {mapState === 'browsing' && allLocations.length !== 0 && (
+      {/* {mapState === 'browsing' && allLocations.length !== 0 && (
         <SearchPlaces points={points} onSetPoints={setPoints} allLocations={allLocations} />
       )}
 
@@ -175,9 +175,9 @@ export const Map = () => {
 
       {mapState === 'navigating' && pathInfo && (
         <NavigationPanel pathInfo={pathInfo} setMapState={setMapState} />
-      )}
+      )} */}
 
-      <MapContainer
+      {/* <MapContainer
         center={MAP_CENTER}
         maxBounds={L.latLngBounds(BORDER_SW, BORDER_NE)}
         maxBoundsViscosity={MAP_MAX_BOUNDS_VISCOSITY}
@@ -197,7 +197,7 @@ export const Map = () => {
         <Polyline positions={pathInfo.path} />
 
         <OnMapClick />
-      </MapContainer>
+      </MapContainer> */}
     </div>
   );
 };
