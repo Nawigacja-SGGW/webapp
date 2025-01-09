cb549747e5a07f554907fc5dd007767545771201
Przemysław <s205853@sggw.edu.pl>
2025-01-09 22:05:50 +0100

    style: RankingComponent designed

--- src/pages/RankingPage/RankingPage.tsx
+++ src/pages/RankingPage/RankingPage.tsx
@@ -1,5 +1,6 @@
 import { RankingComponent } from './components/RankingComponent';
 import './RankingPage.scss';
+import { Podium } from '@styled-icons/ionicons-outline/Podium';
 
 // Dane wejściowe
 const topUsers = [
@@ -14,8 +15,13 @@ const userPosition = 8; // Pozycja aktualnego użytkownika
 
 export function RankingPage() {
   return (
-    <div>
+    <div className="Main">
+      <div className="Icon">
+        <Podium size={80} />
+      </div>
       <div className="RankingComponent">
+        <RankingComponent topUsers={topUsers} userPosition={userPosition} />
+        <RankingComponent topUsers={topUsers} userPosition={userPosition} />
         <RankingComponent topUsers={topUsers} userPosition={userPosition} />
       </div>
     </div>
--- src/pages/RankingPage/components/RankingComponent.tsx
+++ src/pages/RankingPage/components/RankingComponent.tsx
@@ -14,11 +14,15 @@ export function RankingComponent({ topUsers, userPosition }: RankingProps) {
   return (
     <div className="ranking-container">
       <div className="ranking-top">
-        <h2>Users with the most visits in one place:</h2>
-        <div>
+        <div className="ranking-top2">
+          <h2>Users with the most visits in one place:</h2>
+        </div>
+
+        <hr></hr>
+        <div className="topUsers">
           {topUsers.map((user) => (
             <div className="user">
-              <div className="user-position">{user.position}</div>
+              <div className="user-position">{user.position}.</div>
               <div className="user-name">{user.name}</div>
             </div>
           ))}
