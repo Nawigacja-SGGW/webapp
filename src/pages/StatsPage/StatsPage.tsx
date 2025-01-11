679d384d53097b4b9fa4d640533890e6011f750d
michalmlk <michal.melka1@gmail.com>
2025-01-11 15:25:53 +0100

    statistics page

--- src/pages/StatsPage/StatsPage.tsx
+++ src/pages/StatsPage/StatsPage.tsx
@@ -1,9 +1,34 @@
-import { PageContentWrapper } from '../../layouts/PageContentWrapper/PageContentWrapper.tsx';
+import { StackedLineChart } from 'styled-icons/material-outlined';
+import styles from './StatsPage.module.scss';
+import { Card } from '../../components/ui/Card/Card.tsx';
+import { useTranslation } from 'react-i18next';
 
 export const StatsPage = () => {
+  const { t } = useTranslation();
   return (
-    <PageContentWrapper>
-      <h1> Stats page</h1>
-    </PageContentWrapper>
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <StackedLineChart size={50} />
+        <div className={styles.distanceInfo}>
+          <h2>Your distance covered</h2>
+          <p>0 km</p>
+        </div>
+      </div>
+      <div className={styles.statisticsSection}>
+        <Card heading={t('statisticsPage.top5')}>
+          <ol className={styles.top5list}>
+            <li>WZiM</li>
+            <li>WZiM</li>
+            <li>WZiM</li>
+            <li>WZiM</li>
+            <li>WZiM</li>
+          </ol>
+        </Card>
+        <Card heading={t('statisticsPage.numberOfVisitedLocations')}>
+          <div className={styles.visitedLocations}>5</div>
+        </Card>
+      </div>
+      <div className={styles.footer}></div>
+    </div>
   );
 };
