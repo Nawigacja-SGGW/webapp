507d40ff20dd25f16e1987b470a12c9f55ca4a19
przemekpia <przemekpia@gmail.com>
2025-01-12 15:27:10 +0100

    style: website responsiveness improved

--- src/pages/RankingPage/RankingPage.tsx
+++ src/pages/RankingPage/RankingPage.tsx
@@ -1,8 +1,8 @@
-import { RankingComponent } from './components/RankingComponent';
 import './RankingPage.scss';
+import { RankingComponent } from './components/RankingComponent';
+import { useTranslation } from 'react-i18next';
 import { Podium } from '@styled-icons/ionicons-outline/Podium';
 
-// Dane wejściowe
 const topUsers = [
   { name: 'Anna Nowak', position: 1 },
   { name: 'Krzysztof Kowalski', position: 2 },
@@ -11,18 +11,42 @@ const topUsers = [
   { name: 'Piotr Piotrowski', position: 5 },
 ];
 
-const userPosition = 8; // Pozycja aktualnego użytkownika
+const userPosition = 8;
 
 export function RankingPage() {
+  const { t } = useTranslation();
   return (
     <div className="main">
       <div className="topbar">
-        <Podium size={80} />
+        <Podium className="icon" />
       </div>
       <div className="rankings">
-        <RankingComponent topUsers={topUsers} userPosition={userPosition} />
-        <RankingComponent topUsers={topUsers} userPosition={userPosition} />
-        <RankingComponent topUsers={topUsers} userPosition={userPosition} />
+        <div className="rankings-top">
+          <div>
+            <RankingComponent
+              topUsers={topUsers}
+              userPosition={userPosition}
+              title={t('RankingPage.user.onePlace')}
+            />
+          </div>
+          <div>
+            <RankingComponent
+              topUsers={topUsers}
+              userPosition={userPosition}
+              title={t('RankingPage.user.mostPlaces')}
+            />
+          </div>
+          <div>
+            <RankingComponent
+              topUsers={topUsers}
+              userPosition={userPosition}
+              title={t('RankingPage.user.longestDistance')}
+            />
+          </div>
+        </div>
+        <div className="rankings-bot">
+          <RankingComponent topUsers={topUsers} title={t('RankingPage.campus.mostPlaces')} />
+        </div>
       </div>
     </div>
   );
--- src/pages/RankingPage/components/RankingComponent.tsx
+++ src/pages/RankingPage/components/RankingComponent.tsx
@@ -1,5 +1,6 @@
 import './RankingComponent.scss';
 import { Card } from '../../../components/ui/Card/Card.tsx';
+import { useTranslation } from 'react-i18next';
 
 interface User {
   name: string;
@@ -8,25 +9,33 @@ interface User {
 
 interface RankingProps {
   topUsers: User[];
-  userPosition: number;
+  userPosition?: number;
+  title: string;
 }
 
-export function RankingComponent({ topUsers, userPosition }: RankingProps) {
+export function RankingComponent({ topUsers, userPosition, title }: RankingProps) {
+  const containerClass =
+    userPosition == null ? 'ranking-container ranking-container-extended' : 'ranking-container';
+  const { t } = useTranslation();
+
   return (
-    <div className="ranking-container">
-      <Card heading="Users with the most visits in one place">
+    <div className={containerClass}>
+      <Card heading={title}>
         <div className="top-users">
           {topUsers.map((user) => (
-            <div className="user">
+            <div className="user" key={user.position}>
               <div className="user-position">{user.position}.</div>
               <div className="user-name">{user.name}</div>
             </div>
           ))}
         </div>
       </Card>
-      <Card heading="Your place in the ranking:">
-        <div className="user-ranking">{userPosition}</div>
-      </Card>
+
+      {userPosition != null && (
+        <Card heading={t('RankingPage.rankingPlace')}>
+          <div className="user-ranking">{userPosition}</div>
+        </Card>
+      )}
     </div>
   );
 }
